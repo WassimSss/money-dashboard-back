cbb917850a6b6a11b4d9eea042515c501a84e990
WassimSss <tojiikan@gmail.com>
2024-04-10 10:45:09 +0200

    setup

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+const User = require('../models/users');
+var router = express.Router();
+
+
+router.post('/register', function (req, res, next) {
+
+    req.body.
+  
+});
+
+module.exports = router;
