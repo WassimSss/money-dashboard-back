d8a55ab492a9e17809d1a67bbe4577b2f7b2738c
Salmi <tojiikan@gmail.com>
2024-04-21 16:15:06 +0200

    merge

--- models/users.js
+++ models/users.js
@@ -1,19 +1,11 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-<<<<<<< HEAD
-    firstname: {
-        type: String,
-        required: true
-    },
-    lastname: {
-=======
     firstName: {
         type: String,
         required: true
     },
     lastName: {
->>>>>>> f7d87f47489b49dfb00c3689e7cb19d0b2c9193c
         type: String,
         required: true
     },
@@ -26,13 +18,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-<<<<<<< HEAD
-=======
     balance: {
         type: Number,
         default: 0
     }
->>>>>>> f7d87f47489b49dfb00c3689e7cb19d0b2c9193c
 });
 
 const User = mongoose.model('User', userSchema);
